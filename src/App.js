de12b36db1096fe2c2774f4d7db108d272d33349
Ronald Hong <ron@wistia.com>
2017-04-19 19:15:50 -0400

    Snip demo code cruft.

--- src/App.js
+++ src/App.js
@@ -3,40 +3,22 @@ import { connect } from 'react-redux';
 import Login from './containers/Login';
 import Channels from './containers/Channels';
 import { getToken } from './store/selectors';
-import logo from './logo.svg';
 import './App.css';
 
 export class App extends Component {
   render() {
-    const { message, token } = this.props;
+    const { token } = this.props;
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>{message}</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <div>
-          <Login />
-        </div>
-
-        <div>
-          <Channels token={token} />
-        </div>
+      <div>
+        {!token && <Login />}
+        {token && <Channels token={token} />}
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  message: 'Welcome to React',
   token: getToken(state),
 })
 
-const mapDispatchToProps = (dispatch) => ({
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
